2c236a80f9348b5b7815da9db03a2171ad9f8e78
Isa-Leal <isakerry@gmail.com>
2024-11-29 01:34:08 -0300

    Segunda atividade: somar dois números

---
+++ atividade02.py
@@ -0,0 +1,17 @@
+# Programa: Solicitar dois números e somá-los
+
+# Função inicio()
+def inicio():
+    # Declarando as variáveis
+    numero1 = float(input("Digite o primeiro número: "))  # Entrada do primeiro número
+    numero2 = float(input("Digite o segundo número: "))  # Entrada do segundo número
+
+    # Calculando a soma
+    soma = numero1 + numero2
+
+    # Exibindo o resultado
+    print(f"A soma dos números é: {soma}")
+
+# Chamada da função inicio()
+inicio()
+
